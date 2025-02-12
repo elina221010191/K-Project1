7227f97389647bec6bb724704047a488a5da9fe5
elina221010191 <niyolina32@gmail.com>
2025-02-12 22:02:17 +0200

    dashboard

--- Reactapp/src/Dashboard/DashboardNavbar.jsx
+++ Reactapp/src/Dashboard/DashboardNavbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Dashboard_Styles/dashboardnavbar.css'
+import { RxSwitch } from "react-icons/rx";
+import { CiSearch } from "react-icons/ci";
 function DashboardNavbar(){
     return(
-        <div>
-            Hello
-                {/* <input type="text" placeholder="Search for stocks..." className="tex"/>
-                <p className="text1">HELLO JOHN</p> */}
+        <div className="textt">
+                <CiSearch /><input type="text" placeholder="Search for stocks..." className="tex"/>
+                <div className="text1">
+                <h1>HELLO JOHN</h1>
+                <h6>Have a nice work</h6>
+                </div>
+                <div className="text1">
+                <h1>Profile Growth <RxSwitch /></h1>
+                        <h6>Overall information</h6>
+                </div>
         </div>
 
     )
--- Reactapp/src/Dashboard/Dashboardview.jsx
+++ Reactapp/src/Dashboard/Dashboardview.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import './Dashboard_Styles/dashboardview.css'
+import { TbHandClick } from "react-icons/tb";
+
 
 const Dashboardview=()=>
 {
+    const cards =[
+        {
+            id:1,
+            title:"New Subscribers",
+            content:"5.093",
+            image:"",
+        },
+        {
+            id:2,
+            title:"Streams",
+            content:"47,095",
+            image:"",
+        },
+        {
+            id:3,
+            title:"Engagement",
+            content:"25.81",
+            image:"",
+        },
+        {
+            id:4,
+            title:"Watch",
+            content:"45.42",
+            image:"",
+        }
+    ];
     return(
-    <div>
-        Helloesdrftgvbhnj
-    </div>
+        <div className="card1-container">
+                            {cards.map((card) =>(
+                                <div key={card.id} className="card">
+                                     
+                                    {/* <img src={card.image} alt="card.title" className="card-imagev"></img> */}
+                                    <h1 className="card-title" ><TbHandClick />{card.title}</h1>
+                                        <p className="card-content">{card.content}</p>
+                                        {/* //<BiSolidChevronRightCircle className="iconv" size={40}/> */}
+                        </div>
+                ))}
+                    </div>
+    
     )
 }
 export default Dashboardview;
